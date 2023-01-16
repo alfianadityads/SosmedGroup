a40723d5f01d81f47454c08fa4e96eddc66bc67a
Fauzi Sofyan <fauzilax@gmail.com>
2023-01-16 23:47:10 +0700

    Chore: Writing some Code method on Handler and Request

--- features/user/handler/handler.go
+++ features/user/handler/handler.go
@@ -1,1 +1,120 @@
 package handler
+
+import (
+	"net/http"
+	"sosmedapps/features/user"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+type userController struct {
+	srv user.UserService
+}
+
+func New(us user.UserService) user.UserHandler {
+	return &userController{
+		srv: us,
+	}
+}
+
+// Register implements user.UserHandler
+func (uc *userController) Register() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := RegisterRequest{}
+		err := c.Bind(&input)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "wrong input format"})
+		}
+
+		res, err := uc.srv.Register(*RequstToCore(input))
+		if err != nil {
+			if strings.Contains(err.Error(), "email") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "email already registered"})
+			} else if strings.Contains(err.Error(), "empty") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "all input must fill"})
+			} else if strings.Contains(err.Error(), "username") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "username already registered"})
+			}
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{"message": "internal server error"})
+		}
+		return c.JSON(http.StatusCreated, map[string]interface{}{
+			"data":    res,
+			"message": "success creating account",
+		})
+
+	}
+}
+
+// Login implements user.UserHandler
+func (uc *userController) Login() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := LoginRequest{}
+		err := c.Bind(&input)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "wrong input format"})
+		}
+		tokenGen, res, err := uc.srv.Login(input.UserName, input.Password)
+		if err != nil {
+			if strings.Contains(err.Error(), "not found") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "account not registered"})
+			} else if strings.Contains(err.Error(), "empty") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "username or password not allowed empty"})
+			} else if strings.Contains(err.Error(), "password") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "wrong password"})
+			} else {
+				return c.JSON(http.StatusInternalServerError, map[string]interface{}{"message": "internal server error"})
+			}
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"data":    res,
+			"token":   tokenGen,
+			"message": "login success",
+		})
+	}
+}
+
+// Update implements user.UserHandler
+func (uc *userController) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := UpdateRequest{}
+		err := c.Bind(&input)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "wrong input format"})
+		}
+		res, err := uc.srv.Update(c.Get("user"), *RequstToCore(input))
+		if err != nil {
+			if strings.Contains(err.Error(), "email") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "email already used"})
+			} else if strings.Contains(err.Error(), "username") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "username already used"})
+			} else {
+				return c.JSON(http.StatusInternalServerError, map[string]interface{}{"message": "internal server error"})
+			}
+		}
+		return c.JSON(http.StatusCreated, map[string]interface{}{
+			"data":    res,
+			"message": "success updating account",
+		})
+	}
+}
+
+// Profile implements user.UserHandler
+func (uc *userController) Profile() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+// Delete implements user.UserHandler
+func (uc *userController) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		err := uc.srv.Delete(c.Get("user"))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"message": "internal server error, account fail to delete",
+			})
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "deleting account successful",
+		})
+	}
+}
--- features/user/handler/request.go
+++ features/user/handler/request.go
@@ -1,1 +1,54 @@
 package handler
+
+import (
+	"sosmedapps/features/user"
+)
+
+type LoginRequest struct {
+	UserName string `json:"email" form:"username"`
+	Password string `json:"password" form:"password"`
+}
+
+type RegisterRequest struct {
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	UserName string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+}
+
+type UpdateRequest struct {
+	Name        string `json:"name" form:"name"`
+	Email       string `json:"email" form:"email"`
+	Image       string `json:"file" form:"file"`
+	UserName    string `json:"username" form:"username" validate:"required"`
+	Password    string `json:"password" form:"password"`
+	DateOfBirth string `json:"dateofbirth" form:"dateofbirth"`
+}
+
+func RequstToCore(dataUser interface{}) *user.Core {
+	res := user.Core{}
+	switch dataUser.(type) {
+	case LoginRequest:
+		cnv := dataUser.(LoginRequest)
+		res.UserName = cnv.UserName
+		res.Password = cnv.Password
+	case RegisterRequest:
+		cnv := dataUser.(RegisterRequest)
+		res.Name = cnv.Name
+		res.Email = cnv.Email
+		res.UserName = cnv.UserName
+		res.Password = cnv.Password
+	case UpdateRequest:
+		cnv := dataUser.(UpdateRequest)
+		res.Name = cnv.Name
+		res.Email = cnv.Email
+		res.Image = cnv.Image
+		res.UserName = cnv.UserName
+		res.Password = cnv.Password
+		res.DateOfBirth = cnv.DateOfBirth
+	default:
+		return nil
+	}
+	return &res
+
+}
