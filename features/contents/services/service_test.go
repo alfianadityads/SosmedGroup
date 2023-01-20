1320f308bece3a9dca55cf0c65309ca2d2680510
icxz1 <alfianadityadwi@gmail.com>
2023-01-20 08:15:20 +0700

    chore: delete unused unit test func in del content

--- features/contents/services/service_test.go
+++ features/contents/services/service_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"sosmedapps/features/contents"
 	"sosmedapps/helper"
 	"sosmedapps/mocks"
 	"testing"
@@ -26,20 +27,6 @@ func TestDelete(t *testing.T) {
 	})
 
 
-	// t.Run("data not found", func(t *testing.T) {
-	// 	repo.On("DeleteContent", uint(7), uint(1)).Return(errors.New("data not found")).Once()
-
-	// 	srv := New(repo)
-
-	// 	_, token := helper.GenerateToken(7)
-	// 	pToken := token.(*jwt.Token)
-	// 	pToken.Valid = true
-	// 	err := srv.DeleteContent(pToken, 1)
-	// 	assert.NotNil(t, err)
-	// 	assert.ErrorContains(t, err, "query error")
-	// 	repo.AssertExpectations(t)
-	// })
-
 	t.Run("internal server error", func(t *testing.T) {
 		repo.On("DeleteContent", uint(1), uint(1)).Return(errors.New("server error")).Once()
 		srv := New(repo)
@@ -50,6 +37,25 @@ func TestDelete(t *testing.T) {
 		err := srv.DeleteContent(pToken, 1)
 		assert.NotNil(t, err)
 		assert.ErrorContains(t, err, "server error")
+		repo.AssertExpectations(t)
+	})
+}
+
+
+func TestUpdateContent(t *testing.T) {
+	repo := mocks.NewContentData(t)
+	inputData := contents.CoreContent{ID: uint(1), Content: "hello everyone"}
+	resData := contents.CoreContent{ID: uint(1),Content: "hello everybody"}
+
+	t.Run("success update content", func(t *testing.T) {
+		repo.On("UpdateContent", uint(1), uint(1), inputData).Return(resData, nil).Once()
+		srv := New(repo)
+		_, tokenIDUser := helper.GenerateToken(1)
+		userID := tokenIDUser.(*jwt.Token)
+		userID.Valid = true
+		_, err := srv.UpdateContent(userID, uint(1), "hello everyone")
+		assert.Nil(t, err)
+		assert.Equal(t, resData.ID, userID)
 		repo.AssertExpectations(t)
 	})
 }
